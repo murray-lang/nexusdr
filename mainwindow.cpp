6e3972c0dd82d6c37a7d000efc03428d22a5b42a
Murray Lang <murray.lang@westnet.com.au>
2026-01-14 19:01:08 +0800

    MainWindow Event handler closes toolbar button popups

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -53,7 +53,7 @@ MainWindow::MainWindow(RadioConfig& radioConfig, QWidget *parent)
 
   if (this->centralWidget()) {
     // This filter has been added to help manage popup behaviour associated with the toolbar buttons.
-    // this->centralWidget()->installEventFilter(this);
+    this->installEventFilter(this);
   }
 }
 
@@ -173,7 +173,7 @@ MainWindow::on_actionBand_triggered()
     // QWidget* centralWidget = this->centralWidget();
     QWidget* existing = findChild<QWidget*>("bandPanel");
     if (existing) {
-      existing->deleteLater();
+      existing->close();
       return;
     }
     auto* panel = new QtBandDialog(m_pRadio, this);
@@ -204,7 +204,7 @@ MainWindow::closeActiveToolbarPopups()
   const auto panels = this->centralWidget()->findChildren<QWidget*>();
   for (auto* panel : panels) {
     if (panel->property(toolbarPopupPropertyName).toBool()) {
-      panel->deleteLater();
+      panel->close();
     }
   }
 }
@@ -212,20 +212,20 @@ MainWindow::closeActiveToolbarPopups()
 bool
 MainWindow::eventFilter(QObject *watched, QEvent *event)
 {
-  // if (watched == this->centralWidget() && event->type() == QEvent::MouseButtonPress) {
-  //   auto* mouseEvent = static_cast<QMouseEvent*>(event);
-  //
-  //   const auto panels = this->centralWidget()->findChildren<QWidget*>();
-  //   for (auto* panel : panels) {
-  //     if (panel->property(toolbarPopupPropertyName).toBool() && panel->isVisible()) {
-  //       // If the click is outside this panel, close it
-  //       if (!panel->geometry().contains(mouseEvent->pos())) {
-  //         panel->deleteLater();
-  //         return true; // Consume the event so it doesn't click the UI behind
-  //       }
-  //     }
-  //   }
-  // }
+  if (watched == this && event->type() == QEvent::MouseButtonPress) {
+    auto* mouseEvent = static_cast<QMouseEvent*>(event);
+
+    const auto panels = this->findChildren<QWidget*>();
+    for (auto* panel : panels) {
+      if (panel->property(toolbarPopupPropertyName).toBool() && panel->isVisible()) {
+        // If the click is outside this panel, close it
+        if (!panel->geometry().contains(mouseEvent->pos())) {
+          panel->close();
+          return true; // Consume the event so it doesn't click the UI behind
+        }
+      }
+    }
+  }
   return QMainWindow::eventFilter(watched, event);
 }
 
